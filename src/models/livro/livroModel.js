89b3f91f02f85ac876f2f317a762058c66158664
GabrielRRamirez <gabriel.r.ramirez39@hotmail.com>
2023-04-10 13:38:39 -0300

    Estudo sobre validadores personalizados com mongoose

--- src/models/livro/livroModel.js
+++ src/models/livro/livroModel.js
@@ -2,19 +2,33 @@ import mongoose from "mongoose";
 
 let livroSchema = mongoose.Schema(
     {
-        id: {type: String},
+        id: { type: String },
         titulo: {
-            type: String, 
+            type: String,
             required: [true, "O título é um campo obrigatório"]
         },
         valor: {
-            type: Number, 
-            required: [true, "O valor é um campo obrigatório"]
+            type: Number,
+            required: [true, "O valor é um campo obrigatório"],
+            validate: {
+                validator: (value) => {
+                    return value >= 10 && value <= 50000.0;
+                },
+                message: "O valor deve estar entre R$10,00 e R$50.000,00"
+            }
         },
         autor: {
-            type: mongoose.Schema.Types.ObjectId, 
-            ref:"autor", 
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "autor",
             required: [true, "Autor é um campo obrigatório"]
+        },
+        editora: {
+            type: String,
+            required: [true, "A editora é um campo obrigatório"],
+            enum: {
+                values: ["Casa do código", "Alura", "Dev Dojo"],
+                message: "A Editora informada não é válida"
+            }
         }
     }
 );
